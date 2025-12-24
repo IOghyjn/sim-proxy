f9d92bebb83d983142a54f845251d97dc35741ed
EL MAAKOUL MOUSTAPHA <mostafaelmakoul@gmail.com>
2025-12-24 13:12:39 +0000

    Refactor response handling in index.js
    Updated response handling to use sendStatus for simplicity.

--- index.js
+++ index.js
@@ -8,27 +8,19 @@ app.use(express.json({ limit: '10mb' }));
 
 app.post('/upload_data', async (req, res) => {
   try {
-    const response = await axios.post(
-      'https://iot.pcs-agri.com/upload_data',
-      req.body,
-      { maxRedirects: 0 }
-    );
-    res.status(response.status).send('OK');
-  } catch (e) {
-    res.status(500).send('Erreur proxy');
+    const response = await axios.post('https://iot.pcs-agri.com/upload_data', req.body, { maxRedirects: 0 });
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(500);
   }
 });
 
 app.post('/upload_chunk', async (req, res) => {
   try {
-    const response = await axios.post(
-      'https://iot.pcs-agri.com/upload_chunk',
-      req.body,
-      { maxRedirects: 0 }
-    );
-    res.status(response.status).send('OK');
-  } catch (e) {
-    res.status(500).send('Erreur chunk');
+    const response = await axios.post('https://iot.pcs-agri.com/upload_chunk', req.body, { maxRedirects: 0 });
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(500);
   }
 });
 
